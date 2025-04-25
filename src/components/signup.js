579eefe1b51c4bdbe2c83ff5302c06eadf03effb
Hakankrc <150073725+Hakankrc@users.noreply.github.com>
2025-04-25 15:54:10 +0300

    added user interaction

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,22 +1,53 @@
 import React from 'react';
-import { 
-  Grid, 
-  Paper, 
-  Avatar, 
-  TextField, 
-  Button, 
-  FormControlLabel, 
+import {
+    Grid,
+    Paper,
+    Avatar,
+    TextField,
+    Button,
+    FormControlLabel,
     Typography,
-  Checkbox 
+    Checkbox
 } from '@mui/material';
+
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import {FormHelperText} from '@mui/material';
+import * as Yup from 'yup';
 
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import '../style/Style.css';
 
 const Signup = () => {
 
-    const marginTop = { marginTop: 20 }
-    const marginButton = { marginTop: 30}
+    const marginTop = { marginTop: 5}
+    const marginButton = { marginTop: 5 }
+    const initialValues = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        termsAndConditions: false
+    }
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().min(4, "çok kısa").required('Kullanıcı adı zorunludur'),
+        email: Yup.string().email('Geçersiz e-posta adresi').required('E-posta adresi zorunludur'),
+        password: Yup.string().min(6, 'Şifre en az 6 karakter olmalıdır').required('Şifre zorunludur'),
+        confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Şifreler eşleşmiyor').required('Şifreyi onaylamak zorunludur'),
+        termsAndConditions: Yup.boolean().oneOf([true], 'Hizmet şartlarını kabul etmelisiniz')
+    })
+
+
+
+    const onSubmit = (values, props) => {
+        console.log(values);
+        console.log(props);
+        setTimeout(() => {
+            props.setSubmitting(false);
+            props.resetForm();
+            alert("Kayıt işlemi başarılı");
+        }, 2000);
+
+    }
     return (
         <Grid>
             <Paper className='paperStyle' >
@@ -26,21 +57,30 @@ const Signup = () => {
                     </Avatar>
                     <Typography variant="h5" className="headerStyle">Kayıt Ol</Typography>
 
-                    
+
                 </Grid>
-                <form>
-                    <TextField style={marginTop} fullWidth label='Kullanıcı Adı' placeholder="Bir kullanıcı adı girin" required />
-                    <TextField style={marginTop} fullWidth label='Mail Adresi' placeholder="Bir mail adresi girin" required 
-                    />                   
-                    <TextField style={marginTop} type='password' fullWidth label='Şifre' placeholder="Şifrenizi belirleyin" required/>
-                    <TextField style={marginTop} type='password' fullWidth label='Şifreyi Onayla' placeholder="Belirlediğiniz şifreyi onaylayın" required />
-                    <FormControlLabel style={marginButton}
-                        control={<Checkbox name="checkedA" />}
-                        label="Hizmet şartlarını kabul ediyorum."
-                        
-                    />
-                    <Button className='customButton' type='submit' variant='contained' >Hesap Oluştur</Button>
-                </form>
+                <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                    {(props) => (
+                        <Form>
+
+                            <Field as={TextField} style={marginTop} name='name' helperText={<ErrorMessage name='name' />} fullWidth label='Kullanıcı Adı' placeholder="Bir kullanıcı adı girin" required />
+                            <Field as={TextField} style={marginTop} name='email' helperText={<ErrorMessage name='email' />} fullWidth label='Mail Adresi' placeholder="Bir mail adresi girin" required
+                            />
+                            <Field as={TextField} style={marginTop} name='password' helperText={<ErrorMessage name='password' />} type='password' fullWidth label='Şifre' placeholder="Şifrenizi belirleyin" required />
+                            <Field as={TextField} style={marginTop} name='confirmPassword' helperText={<ErrorMessage name='confirmPassword' />} type='password' fullWidth label='Şifreyi Onayla' placeholder="Belirlediğiniz şifreyi onaylayın" required />
+                            <FormControlLabel style={marginButton} 
+                                control={<Field as={Checkbox}  name="termsAndConditions" />}
+                                label="Hizmet şartlarını kabul ediyorum."
+
+                            />
+                            <FormHelperText error>
+                                <ErrorMessage name='termsAndConditions' />
+                            </FormHelperText>
+                            <Button className='customButton' type='submit' disabled={props.isSubmitting} variant='contained' >{props.isSubmitting? "Gönderiliyor":"Kayıt ol"}</Button>
+
+                        </Form>
+                    )}
+                </Formik>
             </Paper>
         </Grid>
     )
