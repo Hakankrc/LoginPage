6ca96a6afac34a54b7f674c3b1660e17113382e9
Hakankrc <150073725+Hakankrc@users.noreply.github.com>
2025-04-25 11:37:03 +0300

    folder structure fixed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SignInOutContainer from './containers/index.js';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <SignInOutContainer />
+
+
+    </div>
+  );
+}
+
+export default App;
